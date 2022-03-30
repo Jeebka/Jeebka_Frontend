3faa2a3f6f6de60605b33d247ea2e61b72aa6210
Luis Amaya <luis.amaya-o@mail.escuelaing.edu.co>
2022-03-30 15:26:19 -0500

    AddLink and SearchBar completed. Use states, not that good

--- components/links/AddLink.js
+++ components/links/AddLink.js
@@ -2,21 +2,29 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import TextField from "@mui/material/TextField";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
-
+import { CircularProgress } from "@mui/material";
+import { Autocomplete } from "@mui/material";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
+import Checkbox from "@mui/material/Checkbox";
 import css from "../../styles/Link.module.css";
 
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import CheckboxesTags from "./CheckboxesTag";
-import GroupsAdd from "./GroupsAdd";
 import SendIcon from "@mui/icons-material/Send";
 
+function sleep(delay = 0) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay);
+  });
+}
+
 function AddLink() {
+  const [tagsAdd, setTagsAdd] = useState({});
+  const [groupAdd, setGroupAdd] = useState(" ");
   const [link, setLink] = useState({
     name: "",
     url: "",
-    tags: [],
-    group: "undefined",
   });
 
   const handleChange = (e) => {
@@ -27,14 +35,42 @@ function AddLink() {
   };
 
   const addLink = () => {
-    console.log(link);
+    console.log(link, "tags:", tagsAdd, "group:", groupAdd);
   };
 
   const [show, setShow] = useState(false);
-
+  const [open, setOpen] = useState(false);
+  const [options, setOptions] = useState([]);
   const handleClick = () => {
     setShow(!show);
   };
+
+  const loading = open && options.length === 0;
+
+  React.useEffect(() => {
+    let active = true;
+    if (!loading) {
+      return undefined;
+    }
+    (async () => {
+      await sleep(1e3);
+      if (active) {
+        setOptions([...groups]);
+      }
+    })();
+    return () => {
+      active = false;
+    };
+  }, [loading]);
+
+  React.useEffect(() => {
+    if (!open) {
+      setOptions([]);
+    }
+  }, [open]);
+
+  const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+  const checkedIcon = <CheckBoxIcon fontSize="small" />;
 
   return (
     <div>
@@ -77,11 +113,69 @@ function AddLink() {
               onChange={handleChange}
               sx={{ width: 297 }}
             />
-            <GroupsAdd width={610} value={link.group} onChange={handleChange} />
-            <CheckboxesTags
-              width={610}
-              value={link.tags}
-              onChanges={handleChange}
+            <Autocomplete
+              id="asynchronous-group-addlink"
+              name="group"
+              open={open}
+              size="medium"
+              sx={{ width: 610 }}
+              onChange={(event, value) => setGroupAdd(value)}
+              onOpen={() => {
+                setOpen(true);
+              }}
+              onClose={() => {
+                setOpen(false);
+              }}
+              isOptionEqualToValue={(option, value) =>
+                option.name === value.name
+              }
+              getOptionLabel={(option) => option.name}
+              options={options}
+              loading={loading}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Groups"
+                  InputProps={{
+                    ...params.InputProps,
+                    endAdornment: (
+                      <React.Fragment>
+                        {loading ? (
+                          <CircularProgress color="inherit" size={20} />
+                        ) : null}
+                        {params.InputProps.endAdornment}
+                      </React.Fragment>
+                    ),
+                  }}
+                />
+              )}
+            />
+
+            <Autocomplete
+              multiple
+              name="tags"
+              size="medium"
+              //error={error}
+              id="checkbox-tags"
+              sx={{ width: 610 }}
+              options={tags}
+              disableCloseOnSelect
+              getOptionLabel={(tags) => tags.name}
+              onChange={(event, value) => setTagsAdd(value)}
+              renderOption={(props, tags, { selected }) => (
+                <li {...props}>
+                  <Checkbox
+                    icon={icon}
+                    checkedIcon={checkedIcon}
+                    style={{ marginRight: 3 }}
+                    checked={selected}
+                  />
+                  {tags.name}
+                </li>
+              )}
+              renderInput={(params) => (
+                <TextField {...params} label="Tags" placeholder="Tags" />
+              )}
             />
 
             <Button
@@ -97,5 +191,23 @@ function AddLink() {
     </div>
   );
 }
+
+const groups = [
+  { name: "Mate", id: "123" },
+  { name: "Sova", id: "321" },
+];
+
+const tags = [
+  { name: "Mate" },
+  { name: "Algebra" },
+  { name: "ECI" },
+  { name: "Games" },
+  { name: "Doom" },
+  { name: "Algoritmos" },
+  { name: "Redes" },
+  { name: "Web" },
+  { name: "HTML" },
+  { name: "CSS" },
+];
 
 export default AddLink;
--- components/links/SearchBar.js
+++ components/links/SearchBar.js
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
 import { Box } from "@mui/material";
-import CheckboxesTags from "./CheckboxesTag";
 import TextField from "@mui/material/TextField";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
+import Autocomplete from "@mui/material/Autocomplete";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
+import Checkbox from "@mui/material/Checkbox";
+
 import css from "../../styles/Link.module.css";
 
 function SearchBar() {
-  const [searchValue, setSearchValue] = useState({ link: "", tags: [] });
+  const [searchValue, setSearchValue] = useState({ link: "" });
+  const [searchTags, setSearchTags] = useState({});
+  const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+  const checkedIcon = <CheckBoxIcon fontSize="small" />;
   const handleChange = (e) => {
     setSearchValue({
       ...searchValue,
@@ -15,8 +22,21 @@ function SearchBar() {
     });
   };
 
+  const tags = [
+    { name: "Mate" },
+    { name: "Algebra" },
+    { name: "ECI" },
+    { name: "Games" },
+    { name: "Doom" },
+    { name: "Algoritmos" },
+    { name: "Redes" },
+    { name: "Web" },
+    { name: "HTML" },
+    { name: "CSS" },
+  ];
+
   const searchLink = () => {
-    console.log(searchValue);
+    console.log(searchValue, searchTags);
   };
 
   return (
@@ -33,6 +53,7 @@ function SearchBar() {
       >
         <TextField
           sx={{ width: 300 }}
+          size="small"
           required
           className="searchLink"
           name="link"
@@ -41,16 +62,34 @@ function SearchBar() {
           value={searchValue.link}
           onChange={handleChange}
         />
-        <CheckboxesTags
-          width={230}
-          onChange={handleChange}
-          prevState={searchValue.tags}
+        <Autocomplete
+          multiple
+          name="tags"
+          size="small"
+          //error={error}
+          id="checkbox-tags"
+          sx={{ width: 230 }}
+          options={tags}
+          disableCloseOnSelect
+          getOptionLabel={(tags) => tags.name}
+          onChange={(event, value) => setSearchTags(value)}
+          renderOption={(props, tags, { selected }) => (
+            <li {...props}>
+              <Checkbox
+                icon={icon}
+                checkedIcon={checkedIcon}
+                style={{ marginRight: 3 }}
+                checked={selected}
+              />
+              {tags.name}
+            </li>
+          )}
+          renderInput={(params) => (
+            <TextField {...params} label="Tags" placeholder="Tags" />
+          )}
         />
-        <IconButton>
-          <SearchIcon
-            sx={{ width: 30, height: 30, right: 20 }}
-            onClick={searchLink}
-          />
+        <IconButton onClick={searchLink}>
+          <SearchIcon sx={{ width: 30, height: 30, right: 20 }} />
         </IconButton>
       </Box>
     </div>
